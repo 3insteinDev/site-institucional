08b0856b9f3c14c2d8b36f1bd5dda6fda02feea0
einstein.soares <einstein.soares@consultoriafr.com.br>
2025-07-30 09:47:49 -0300

    Inicio do projeto institucional com React e Vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Site Institucional</h1>
+      <p>Este é um projeto em React com Vite!</p>
+    </div>
+  );
+}
+
+export default App;
